d37a83638119bea5331ba902dbd8b2f6a6f6f867
ChekHub <scantu@mit.edu>
2019-06-26 18:05:09 -0400

    Create matplotlib_styles.py

---
+++ matplotlib_styles.py
@@ -0,0 +1,18 @@
+font.family : sans-serif
+
+axes.facecolor : white
+axes.prop_cycle : cycler('color',['e41a1c', '377eb8', '4daf4a', '984ea3', 'ff7f00', 'ffff33', 'a65628', 'f781bf'])
+axes.linewidth : 0.0
+axes.grid : True
+
+lines.linewidth : 1.5
+
+xtick.direction : in
+ytick.direction : in
+
+grid.color : c7dedf
+grid.linestyle : -
+grid.linewidth : 0.3
+
+figure.figsize : 10, 5
+legend.loc : best
